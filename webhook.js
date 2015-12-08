df5f46caeb262d799bfb2ef77f1f590811cbd615
Dominic Shelton <frogamic@gmail.com>
2015-12-09 04:09:42 +1300

    Webhook now listens on PORT

--- webhook.js
+++ webhook.js
@@ -1,8 +1,13 @@
 var express = require('express');
 
+var port = process.env.PORT || 3000;
+
 var app = express();
 
 app.post('/', function (req, res) {
-    res.json({text:"Hello World"});
+    res.json({text:'Hello World'});
 });
+
+app.listen(port);
+console.info('Listening on port ' + port);
 
