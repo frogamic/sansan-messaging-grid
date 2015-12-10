13aa57fd0080321682a3df1378820635ff2d9206
Dominic Shelton <frogamic@gmail.com>
2015-12-11 02:57:05 +1300

    Used search-index for finding cards by name
    Added functions to nrdb for finding cards

--- nrdb-local.js
+++ nrdb-local.js
@@ -1,38 +1,96 @@
 'use strict';
 var request = require('request');
 var schedule = require('node-schedule');
+var si = require('search-index')({
+    deletable:false,
+    fieldsToStore:['id', 'title'],
+    nGramLength: {gte: 1, lte: 3}
+});
 
 var netrunnerdbURL = 'http://netrunnerdb.com/api/cards/';
+var cards = {};
+var scheduledUpdate;
+var initPromise;
+var indexPromise;
 
 module.exports = {
-    init: function () {
-        var self = this;
-        return new Promise (function (resolve, reject) {
-            request(netrunnerdbURL, function (error, response, body) {
-                if (!error && response.statusCode === 200) {
-                    var date = Date(response.headers.expires);
-                    self.cards = JSON.parse(body);
-                    if (self.updater) {
-                        self.updater.cancel();
-                    }
-                    console.log("Fetched card DB");
-                    self.updater = schedule.scheduleJob(date, self.init);
-                    for (var c in self.cards) {
-                        self.cardsByCode[self.cards[c].code] = self.cards[c];
-                    }
-                    resolve();
+    init: init,
+    getCardByTitle: getCardByTitle,
+    getCardByCode: getCardByCode
+}
+
+function getCardByCode (code) {
+    return new Promise (function (resolve, reject) {
+        initPromise.then(function () {
+            if (cards[code]) {
+                resolve(cards[code]);
+            } else {
+                reject();
+            }
+        });
+    });
+}
+
+function getCardByTitle (text) {
+    return new Promise (function (resolve, reject) {
+        indexPromise.then(function () {
+            si.search({query:  {title: [text]}}, function (err, searchResults) {
+                if (err) {
+                    reject(err);
                 } else {
-                    console.error("Failed to fetch card db: error %s", response.statusCode);
-                    reject (error);
+                    if(searchResults.totalHits > 0) {
+                        resolve(cards[searchResults.hits[0].code]);
+                    } else {
+                        reject('no hits');
+                    }
                 }
             });
         });
-    },
-    getCardByTitle: function () {
-        return null;
-    },
-    cards: null,
-    cardsByCode: {},
-    updater: null
+    });
 }
 
+function init (cardArray) {
+    initPromise = new Promise (function (resolve, reject) {
+        if (cardArray) {
+            console.log('Used passed card DB');
+            resolve(cardArray);
+        } else {
+            request(netrunnerdbURL, function (error, response, body) {
+                if (!error && response.statusCode === 200) {
+                    var date = new Date(response.headers.expires);
+                    cardArray = JSON.parse(body);
+                    if (scheduledUpdate) {
+                        scheduledUpdate.cancel();
+                    }
+                    console.log('Fetched card DB, valid until '+date);
+                    scheduledUpdate = schedule.scheduleJob(date, init);
+                    resolve(cardArray);
+                } else {
+                    console.error('Failed to fetch card DB: error %u', response.statusCode);
+                    reject (error);
+                }
+            });
+        }
+    });
+    indexPromise = new Promise (function (resolve, reject) {
+        initPromise.then(function(cardArray) {
+            for (var i = 0; i < cardArray.length; i++) {
+                cards[cardArray[i].code] = cardArray[i];
+                cards[cardArray[i].code].id = parseInt(cardArray[i].code);
+            }
+            si.empty();
+            si.add(cardArray, {}, function (error) {
+                if (!error){
+                    console.log('Cards added to search-index');
+                    resolve();
+                } else {
+                    console.error('Failed to add cards to search-index');
+                    reject (error);
+                }
+            });
+        }, function (error) {
+            reject (error);
+        });
+    });
+    return indexPromise;
+}
--- tests/nrdb-local.js
+++ tests/nrdb-local.js
@@ -4,6 +4,7 @@ var expect = chai.expect;
 var should = chai.should();
 
 var nrdb = require('../nrdb-local');
+var cards = require('../cards.json'); // Use local copy instead of fetching a fresh copy
 
 describe('NetrunnerDB object', function () {
     it('should be an object', function (done) {
@@ -11,33 +12,21 @@ describe('NetrunnerDB object', function () {
         done();
     });
     it('should contain methods for searching cards', function (done) {
-        expect(nrdb).to.have.all.keys('init', 'cardsByCode', 'getCardByTitle', 'cards', 'updater');
+        expect(nrdb).to.have.all.keys('init', 'getCardByCode', 'getCardByTitle');
         expect(nrdb.init).to.be.a('function');
         expect(nrdb.getCardByTitle).to.be.a('function');
-        expect(nrdb.cards).to.be.null;
-        expect(nrdb.cardsByCode).to.be.an('object');
-        expect(nrdb.cardsByCode).to.be.empty;
-        expect(nrdb.updater).to.be.null;
+        expect(nrdb.getCardByCode).to.be.a('function');
         done();
     });
     it('should be initializable', function () {
-        this.timeout(6000);
-        return nrdb.init();
+        this.timeout(30000);
+        return nrdb.init(cards);
     });
-    it('should contain cards after initializing', function (done) {
-        expect(nrdb.cards).to.be.an('array');
-        expect(nrdb.cardsByCode).to.be.an('object');
-        expect(Object.keys(nrdb.cardsByCode).length).to.equal(nrdb.cards.length);
-        done();
+    it('should contain some known cards', function () {
+        return nrdb.getCardByCode('06088');
     });
-    it('should contain some known cards', function (done) {
-        nrdb.cards.find(function (c) {
-            return c.code === '00005';
-        }).title.should.equal('The Shadow: Pulling the Strings');
-        nrdb.cards.find(function (c) {
-            return c.code === '06088';
-        }).title.should.equal('Executive Boot Camp');
-        done();
+    it('should find cards by title', function () {
+        return nrdb.getCardByTitle('NAPD');
     });
 });
 
