7e915f3db8950b1963e58ae21e457a20770aad79
Dominic Shelton <drdshelton@gmail.com>
2015-12-13 01:50:44 +1300

    Wrote function to get slack-formatted messages from cards

--- formatting.js
+++ formatting.js
@@ -1,4 +1,71 @@
-exports.formatText = function (text) {
+var colours = require('./colours.json');
+
+var stats = [
+    ['baselink', ':link:'],
+    ['cost', ':credit:'],
+    ['strength', ' str'],
+    ['trash', ':trash:'],
+    ['memoryunits', ':mu:'],
+    ['agendapoints', ':agenda:']
+];
+
+exports.formatCards = (cards) => {
+    var o = {text:'', attachments:[]};
+    for (var i = 0; i < cards.length; i++) {
+        var a = {pretext: '', mrkdwn_in: ['text', 'pretext']};
+        var faction = cards[i].faction.replace(/(\s|-).*/, '').toLowerCase();
+        if (i === 0) {
+            o.text = this.formatTitle(cards[0].title, cards[0].url);
+        } else {
+            a.pretext = this.formatTitle(cards[i].title, cards[i].url) + '\n';
+        }
+        a.pretext += '*\u200b' + cards[i].type + '\u200b*';
+        if (cards[i].subtype ) {
+            a.pretext += ' - ' + cards[i].subtype;
+        }
+        a.pretext += ' - :' + faction + ':';
+        if (cards[i].factioncost) {
+            for (var j = 0; j < cards[i].factioncost; j++) {
+                a.pretext += '•';
+            }
+        }
+        a.pretext += '\n';
+        var first = true;
+        if (cards[i].type === 'Agenda') {
+            a.pretext += cards[i].advancementcost + '/';
+        }
+        if (cards[i].type === 'Identity') {
+            a.pretext += cards[i].minimumdecksize + '/';
+            if (cards[i].influencelimit || cards[i].influencelimit === 0) {
+                a.pretext += cards[i].influencelimit;
+            } else {
+                a.pretext += '∞';
+            }
+            first = false;
+        }
+        for (j = 0; j < stats.length; j++) {
+            if (cards[i][stats[j][0]] || cards[i][stats[j][0]] === 0) {
+                if (!first) {
+                    a.pretext += ' - ';
+                }
+                a.pretext += cards[i][stats[j][0]] + stats[j][1];
+                first = false;
+            }
+        }
+        a.pretext = a.pretext.replace(/(\d|X):mu:/gi, function (x) {
+            return x.replace(/(.).*/, ':$1mu:').toLowerCase();
+        });
+        a.color = colours[faction];
+        a.text = this.formatText(cards[i].text);
+        o.attachments.push(a);
+    }
+    return o;
+}
+
+exports.formatText = (text) => {
+    if (!text) return text;
+
+    text = text.replace(/\r\n/g, '\n');
     text = text.replace(/\.\s*(.*?):/g, '.\n$1:');
 
     text = text.replace(/\[Credits\]/g, ':credit:');
@@ -8,7 +75,7 @@ exports.formatText = function (text) {
     text = text.replace(/\[Trash\]/g, ':trash:');
     text = text.replace(/\.\s*\[Subroutine\]/g, '.\n:Subroutine:');
     text = text.replace(/\[Subroutine\]/g, ':subroutine:');
-    text = text.replace(/([1-4X])\[Memory Unit\]/gi, function (x) {
+    text = text.replace(/(\d|X)\[Memory Unit\]/gi, function (x) {
         return x.replace(/(.).*/, ':$1mu:').toLowerCase();
     });
     text = text.replace(/\[Memory Unit\]/g, ':mu:');
@@ -28,8 +95,8 @@ exports.formatText = function (text) {
     return text;
 }
 
-exports.formatTitle = function (title, url) {
-    title = '*\u200b' + title + '\u200b*';
+exports.formatTitle = (title, url) => {
+    title = '*\u200b' + title.replace('♦', '◆') + '\u200b*';
     if (url && url !== '') {
         return '<' + url + '|' + title + '>';
     }
