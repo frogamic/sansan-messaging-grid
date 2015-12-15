4fcc464a0463ca2cd404562df59d088da1c683aa
Dominic Shelton <drdshelton@gmail.com>
2015-12-16 02:50:58 +1300

    Added decklist search functionality

--- nrdb-local.js
+++ nrdb-local.js
@@ -5,7 +5,8 @@ var FuzzySearch = require('fuzzysearch-js');
 var LevenshteinFS = require('fuzzysearch-js/js/modules/LevenshteinFS');
 var IndexOfFS = require('fuzzysearch-js/js/modules/IndexOfFS');
 
-var netrunnerdbURL = 'http://netrunnerdb.com/api/cards/';
+var netrunnerCardURL = 'http://netrunnerdb.com/api/cards/';
+var netrunnerDeckURL = 'http://netrunnerdb.com/api/decklist/';
 var cards = {};
 var fuzzySearch;
 var scheduledUpdate;
@@ -15,7 +16,53 @@ var indexPromise;
 module.exports = {
     init: init,
     getCardByTitle: getCardByTitle,
-    getCardByCode: getCardByCode
+    getCardByCode: getCardByCode,
+    getDecklist: getDecklist
+}
+
+function createDecklistCard (code, quantity) {
+    return new Promise (function (resolve, reject) {
+        getCardByCode(code).then(function (card) {
+            var newCard = {};
+            newCard.type = card.type;
+            newCard.quantity = quantity;
+            newCard.card = card;
+            resolve(newCard);
+        }, reject);
+    });
+}
+
+function getDecklist (id) {
+    return new Promise (function (resolve, reject) {
+        request(netrunnerDeckURL + id, function (error, response, body) {
+            if (error || response.statusCode !== 200) {
+                return reject(error);
+            }
+            var decklist = {cards: {}};
+            var deck = JSON.parse(body);
+            var promises = [];
+            decklist.name = deck.name;
+            decklist.url = netrunnerDeckURL + id;
+            decklist.creator = deck.username;
+            for (var code in deck.cards) {
+                promises.push(createDecklistCard(code, deck.cards[code]));
+            }
+            Promise.all(promises).then(function (values) {
+                for (var i in values) {
+                    if (!decklist.cards[values[i].type]) {
+                        decklist.cards[values[i].type] = [];
+                    }
+                    decklist.cards[values[i].type].push({
+                        quantity: values[i].quantity, card: values[i].card
+                    });
+                }
+                resolve(decklist);
+            }, function (err) {
+                console.log(err);
+                reject (err)
+            });
+        });
+    });
 }
 
 function getCardByCode (code) {
@@ -60,7 +107,7 @@ function init (cardArray) {
             console.log('Used passed card DB');
             resolve(cardArray);
         } else {
-            request(netrunnerdbURL, function (error, response, body) {
+            request(netrunnerCardURL, function (error, response, body) {
                 if (!error && response.statusCode === 200) {
                     var date = new Date(response.headers.expires);
                     cardArray = JSON.parse(body);
