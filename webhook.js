a68d37a8f27d6c51ef830dec29130da3dcf515a7
Dominic Shelton <drdshelton@gmail.com>
2015-12-15 15:34:48 +1300

    Added card searching code to POST responder

--- webhook.js
+++ webhook.js
@@ -1,28 +1,53 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var nrdb = require('./nrdb-local');
+var formatting = require('./formatting');
+
 var port = process.env.PORT || 3000;
 
 var app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 
 function findSearchStrings (text) {
+    console.info(text);
     var cardFinder = new RegExp('.*?\\[(.*?)\\]', 'g');
     var searches = [];
     var i;
     while(i = cardFinder.exec(text)) {
         searches.push(i[1]);
     }
+    console.info(searches);
     return searches;
 }
 
 app.post('/', function (req, res) {
-    if (!req.body) {
+    if (!req.body || Object.keys(req.body).length === 0) {
         return res.sendStatus(400);
     }
+
+    if (req.body.user_name === 'slackbot') {
+        return res.sendStatus(200);
+    }
+
+    console.info(req.body);
+
     var searches = findSearchStrings(req.body.text);
     if (searches.length) {
-        res.json({text:'Hello World'});
+        var cards = [];
+        var p = nrdb.getCardByTitle(searches[0]);
+        for (var i = 0; i < searches.length; i++) {
+            p = p.then(function (card) {
+                cards.push(card);
+                return nrdb.getCardByTitle(searches[i]);
+            });
+        }
+        p.then(function (card) {
+            cards.push(card);
+            res.json(formatting.formatCards(cards));
+        }, function () {
+            res.json(formatting.formatCards(cards));
+        });
     } else {
         res.sendStatus(200);
     }
@@ -30,6 +55,8 @@ app.post('/', function (req, res) {
 
 app.listen(port);
 console.info('Express listening on port ' + port);
+
+nrdb.init();
 
 module.exports = {app: app, findSearchStrings: findSearchStrings};
 
