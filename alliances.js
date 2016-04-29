f963ae4765ac02c5734609d6cc61aa4f0d885aea
Dominic Shelton <drdshelton@gmail.com>
2016-04-29 14:59:09 +1200

    Added functions for PAD Factory and Mumbad Virtual Tour

--- alliances.js
+++ alliances.js
@@ -9,12 +9,12 @@ module.exports = {
     10018 : mumbaTemple,
     10019 : museumOfHistory,
     10029 : nonAllianceCards.bind(undefined, "Haas-Bioroid", 6),
-    10038 : undefined,
+    10038 : padFactory,
     10067 : nonAllianceCards.bind(undefined, "Haas-Bioroid", 6),
     10068 : nonAllianceCards.bind(undefined, "Jinteki", 6),
     10071 : nonAllianceCards.bind(undefined, "NBN", 6),
     10072 : nonAllianceCards.bind(undefined, "Weyland Consortium", 6),
-    10076 : undefined,
+    10076 : mumbadVirtualTour,
     10109 : nonAllianceCards.bind(undefined, "NBN", 6)
 };
 
@@ -22,7 +22,7 @@ function nonAllianceCards (faction, minCards, decklist) {
     var factionCards = 0;
     Object.keys(decklist.cards).forEach((type) => {
         decklist.cards[type].forEach((card) => {
-            if (card.card.faction == faction && (!card.card.subtype || card.card.subtype.indexOf("Alliance") == -1)) {
+            if (card.card.faction === faction && (!card.card.subtype || card.card.subtype.indexOf("Alliance") === -1)) {
                 factionCards += card.quantity;
 
             }
@@ -61,6 +61,36 @@ function museumOfHistory (decklist) {
     // Minus 1 for the identity which doesn't count towards the total cards.
     totalCards -= 1;
     if (totalCards >= 50) {
+        return 0;
+    } else {
+        return 1;
+    }
+}
+
+function padFactory (decklist) {
+    var padCampaigns = 0;
+    if (decklist.cards["Asset"]) {
+        decklist.cards["Asset"].forEach(card => {
+            if (card.card.title === "PAD Campaign") {
+                padCampaigns = card.quantity;
+            }
+        });
+    }
+    if (padCampaigns === 3) {
+        return 0;
+    } else {
+        return 1;
+    }
+}
+
+function mumbadVirtualTour (decklist) {
+    var assets = 0;
+    if (decklist.cards["Asset"]) {
+        decklist.cards["Asset"].forEach(card => {
+            assets += card.quantity;
+        });
+    }
+    if (assets >= 7) {
         return 0;
     } else {
         return 1;
