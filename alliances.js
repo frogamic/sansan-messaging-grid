e887b29d6afeec8441af11118333c1282ca38ec4
Dominic Shelton <frogamic@gmail.com>
2016-05-01 13:21:36 +1200

    Added JSdoc to dev-deps and added doc comments to alliances module

--- alliances.js
+++ alliances.js
@@ -1,41 +1,25 @@
-/* alliances.js
- * Written by Dominic Shelton.
+/**
  * Provides a number of functions for calculating the influence cost of alliance cards in decks.
+ * @module alliances
+ * @author Dominic Shelton.
  */
 'use strict';
 
-module.exports = {
-    10013 : nonAllianceCards.bind(undefined, "Jinteki", 6),
-    10018 : mumbaTemple,
-    10019 : museumOfHistory,
-    10029 : nonAllianceCards.bind(undefined, "Haas-Bioroid", 6),
-    10038 : padFactory,
-    10067 : nonAllianceCards.bind(undefined, "Haas-Bioroid", 6),
-    10068 : nonAllianceCards.bind(undefined, "Jinteki", 6),
-    10071 : nonAllianceCards.bind(undefined, "NBN", 6),
-    10072 : nonAllianceCards.bind(undefined, "Weyland Consortium", 6),
-    10076 : mumbadVirtualTour,
-    10109 : nonAllianceCards.bind(undefined, "NBN", 6)
-};
+/**
+ * The function for Heritage Committee is bound to nonAllianceCards with 6 Jinteki cards minimum.
+ * @function 10013
+ * @param   decklist    The decklist to check the influence cost in.
+ * @return  0 if the influence cost in the given decklist is 0, otherwise 1. The result can be multiplied by the regular influence cost.
+ */
+module.exports['10013'] = nonAllianceCards.bind(undefined, "Jinteki", 6);
 
-function nonAllianceCards (faction, minCards, decklist) {
-    var factionCards = 0;
-    Object.keys(decklist.cards).forEach((type) => {
-        decklist.cards[type].forEach((card) => {
-            if (card.card.faction === faction && (!card.card.subtype || card.card.subtype.indexOf("Alliance") === -1)) {
-                factionCards += card.quantity;
-
-            }
-        });
-    });
-    if (factionCards >= minCards) {
-        return 0;
-    } else {
-        return 1;
-    }
-};
-
-function mumbaTemple (decklist) {
+/**
+ * Calculates a multiplier for the influence cost of Mumba Temple by counting the number of ICE in the deck.
+ * @function 10018
+ * @param   decklist    The decklist to calculate the influence for.
+ * @return  0 if the influence cost is free, otherwise 1. The result can be multiplied by the regular influence cost.
+ */
+module.exports['10018'] = (decklist) => {
     var ice = 0;
     ["Barrier", "Code Gate", "Sentry", "Other", "Multi"].forEach((type) => {
         if (decklist.cards[type]) {
@@ -51,7 +35,13 @@ function mumbaTemple (decklist) {
     }
 }
 
-function museumOfHistory (decklist) {
+/**
+ * Calculates a multiplier for the influence cost of Museum of History by counting the number of cards in the deck.
+ * @function    10019
+ * @param   decklist    The decklist to calculate the influence for.
+ * @return  0 if the influence cost is free, otherwise 1. The result can be multiplied by the regular influence cost.
+ */
+module.exports['10019'] = (decklist) => {
     var totalCards = 0;
     Object.keys(decklist.cards).forEach((type) => {
         decklist.cards[type].forEach(card => {
@@ -67,7 +57,21 @@ function museumOfHistory (decklist) {
     }
 }
 
-function padFactory (decklist) {
+/**
+ * The function for Product Recall is bound to nonAllianceCards with 6 Haas-Bioroid cards minimum.
+ * @function 10029
+ * @param   decklist    The decklist to check the influence cost in.
+ * @return  0 if the influence cost in the given decklist is 0, otherwise 1. The result can be multiplied by the regular influence cost.
+ */
+module.exports['10029'] = nonAllianceCards.bind(undefined, "Haas-Bioroid", 6);
+
+/**
+ * Calculates a multiplier for the influence cost of PAD Factory by counting the number of PAD Campaigns in the deck.
+ * @function 10038
+ * @param   decklist    The decklist to calculate the influence for.
+ * @return  0 if the influence cost is free, otherwise 1. The result can be multiplied by the regular influence cost.
+ */
+module.exports['10038'] = (decklist) => {
     var padCampaigns = 0;
     if (decklist.cards["Asset"]) {
         decklist.cards["Asset"].forEach(card => {
@@ -83,7 +87,45 @@ function padFactory (decklist) {
     }
 }
 
-function mumbadVirtualTour (decklist) {
+/**
+ * The function for Jeeves Model Bioroids is bound to nonAllianceCards with 6 Haas-Bioroid cards minimum.
+ * @function 10067
+ * @param   decklist    The decklist to check the influence cost in.
+ * @return  0 if the influence cost in the given decklist is 0, otherwise 1. The result can be multiplied by the regular influence cost.
+ */
+module.exports['10067'] = nonAllianceCards.bind(undefined, "Haas-Bioroid", 6);
+
+/**
+ * The function for Raman Rai is bound to nonAllianceCards with 6 Jinteki cards minimum.
+ * @function 10068
+ * @param   decklist    The decklist to check the influence cost in.
+ * @return  0 if the influence cost in the given decklist is 0, otherwise 1. The result can be multiplied by the regular influence cost.
+ */
+module.exports['10068'] = nonAllianceCards.bind(undefined, "Jinteki", 6);
+
+/**
+ * The function for Salem's Hospitality is bound to nonAllianceCards with 6 NBN cards minimum.
+ * @function 10071
+ * @param   decklist    The decklist to check the influence cost in.
+ * @return  0 if the influence cost in the given decklist is 0, otherwise 1. The result can be multiplied by the regular influence cost.
+ */
+module.exports['10071'] = nonAllianceCards.bind(undefined, "NBN", 6);
+
+/**
+ * The function for Executive Search Firm is bound to nonAllianceCards with 6 Weyland Consortium cards minimum.
+ * @function 10072
+ * @param   decklist    The decklist to check the influence cost in.
+ * @return  0 if the influence cost in the given decklist is 0, otherwise 1. The result can be multiplied by the regular influence cost.
+ */
+module.exports['10072'] = nonAllianceCards.bind(undefined, "Weyland Consortium", 6);
+
+/**
+ * Calculates a multiplier for the influence cost of Mumbad Virtual Tour by counting the number of Assets in the deck.
+ * @function 10038
+ * @param   decklist    The decklist to calculate the influence for.
+ * @return  0 if the influence cost is free, otherwise 1. The result can be multiplied by the regular influence cost.
+ */
+module.exports['10076'] = (decklist) => {
     var assets = 0;
     if (decklist.cards["Asset"]) {
         decklist.cards["Asset"].forEach(card => {
@@ -96,4 +138,36 @@ function mumbadVirtualTour (decklist) {
         return 1;
     }
 }
+
+/**
+ * The function for Ibrahim Salem is bound to nonAllianceCards with 6 NBN cards minimum.
+ * @function 10109
+ * @param   decklist    The decklist to check the influence cost in.
+ * @return  0 if the influence cost in the given decklist is 0, otherwise 1. The result can be multiplied by the regular influence cost.
+ */
+module.exports['10109'] = nonAllianceCards.bind(undefined, "NBN", 6);
+
+/**
+ * Calculates a multiplier for the influence cost of alliance cards that require a set number of in-faction non-alliance cards to be free.
+ * @function    nonAllianceCards
+ * @param   faction     The faction that cards must be to satisfy the requirement. Can any of 'Jinteki', 'Haas-Bioroid', 'NBN', 'Weyland Consortium'.
+ * @param   minCards    The minimum number of such cards that must be in the deck to satisfy the requirement.
+ * @param   decklist    The decklist to check for the requirement.
+ * @return  0 if the influence cost in the given decklist is 0, otherwise 1. The result can be multiplied by the regular influence cost.
+ */
+function nonAllianceCards (faction, minCards, decklist) {
+    var factionCards = 0;
+    Object.keys(decklist.cards).forEach((type) => {
+        decklist.cards[type].forEach((card) => {
+            if (card.card.faction === faction && (!card.card.subtype || card.card.subtype.indexOf("Alliance") === -1)) {
+                factionCards += card.quantity;
+            }
+        });
+    });
+    if (factionCards >= minCards) {
+        return 0;
+    } else {
+        return 1;
+    }
+};
 
