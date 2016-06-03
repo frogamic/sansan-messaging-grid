e11673c59f4fd3aafc9f45292899f805fbe6403b
Dominic Shelton <drdshelton@gmail.com>
2016-06-03 15:50:19 +1200

    Updated to netrunnerDB API version 2.0

--- nrdb-local.js
+++ nrdb-local.js
@@ -10,10 +10,11 @@ var LevenshteinFS = require('fuzzysearch-js/js/modules/LevenshteinFS');
 var IndexOfFS = require('fuzzysearch-js/js/modules/IndexOfFS');
 // var SiftFS = require('fuzzysearch-js/js/modules/Sift3FS');
 
-var netrunnerCardURL = 'https://netrunnerdb.com/api/cards/';
-var netrunnerMWLURL = 'https://netrunnerdb.com/api/get_mwl/';
-var netrunnerDeckURL = 'https://netrunnerdb.com/api/decklist/';
-var netrunnerDeckDisplayURL = 'https://netrunnerdb.com/en/decklist/';
+var nrdbCardURL = 'https://netrunnerdb.com/api/2.0/public/cards';
+var nrdbMWLURL = 'https://netrunnerdb.com/api/2.0/public/mwl';
+var nrdbDeckURL = 'https://netrunnerdb.com/api/2.0/public/decklist/';
+var nrdbDeckDisplayURL = 'https://netrunnerdb.com/en/decklist/';
+var nrdbCardDisplayURL = 'https://netrunnerdb.com/en/card/';
 var cards = {};
 var fuzzySearch;
 var scheduledUpdate;
@@ -42,19 +43,19 @@ function createDecklistCard (code, quantity) {
 
 function getDecklist (id) {
     return new Promise (function (resolve, reject) {
-        request(netrunnerDeckURL + id, function (error, response, body) {
+        request(nrdbDeckURL + id, function (error, response, body) {
             if (error || response.statusCode !== 200) {
                 return reject(error);
             }
             var decklist = {cards: {}};
-            var deck = JSON.parse(body);
+            var deck = JSON.parse(body).data[0];
             var promises = [];
             decklist.name = deck.name;
-            decklist.url = netrunnerDeckDisplayURL + id;
-            decklist.creator = deck.username;
-            for (let code in deck.cards) {
+            decklist.url = nrdbDeckDisplayURL + id;
+            decklist.creator = deck.user_name;
+            Object.keys(deck.cards).forEach((code) => {
                 promises.push(createDecklistCard(code, deck.cards[code]));
-            }
+            });
             Promise.all(promises).then(function (values) {
                 values.forEach((card, i) => {
                     var type = card.card.type;
@@ -132,14 +133,14 @@ function getCardByTitle (text) {
                     //     console.info(i +': ' + result.value.title + ' score: ' + result.score + '\t\t' + details);
                     // });
                     // Return the best hit.
-                    resolve(results[0].value);
+                    getCardByCode(results[0].value.code).then(resolve, reject);
                 }else{
                     // Fall back to an acronym search.
                     var acronym = new RegExp(text.replace(/\W/g, '').replace(/(.)/g, '\\b$1.*?'), 'i');
                     var result = cardArray.find(function (e) {
                         return e.title.match(acronym);
                     });
-                    resolve(result);
+                    getCardByCode(result.code).then(resolve, reject);
                 }
             });
         }
@@ -155,14 +156,14 @@ function init (cardArray) {
             resolve(cardArray);
         } else {
             // Load the json data from netrunnerdb.
-            request(netrunnerCardURL, function (error, response, body) {
+            request(nrdbCardURL, function (error, response, body) {
                 if (error || response.statusCode !== 200) {
                     console.error('Failed to fetch card DB: status', response.statusCode);
                     reject (error);
                 } else {
                     // Find out when the data expires from the HTTP Header.
                     var date = new Date(response.headers.expires);
-                    cardArray = JSON.parse(body);
+                    cardArray = JSON.parse(body).data;
                     // Cancel any other updates as only the newest is necessary.
                     if (scheduledUpdate) {
                         scheduledUpdate.cancel();
@@ -179,7 +180,7 @@ function init (cardArray) {
     var mwlPromise = new Promise ((resolve, reject) => {
         loadPromise.then(() => {
             // Fetch the Most Wanted List from the NetrunnerDB API
-            request(netrunnerMWLURL, (error, response, body) => {
+            request(nrdbMWLURL, (error, response, body) => {
                 if (error || response.statusCode !== 200) {
                     console.error('Failed to fetch Most Wanted List: status', response.statusCode);
                     reject (error);
@@ -188,8 +189,7 @@ function init (cardArray) {
                     var rulesets = JSON.parse(body).data;
                     var latest = {name: "Empty List", start: "1970-01-01", cards: {}};
                     rulesets.forEach((rule) => {
-                        if (rule.name.indexOf("Tournament Rules") === 0 &&
-                                Date.parse(latest.start) < Date.parse(rule.start)) {
+                        if (rule.active) {
                             latest = rule;
                         }
                     });
@@ -203,17 +203,40 @@ function init (cardArray) {
     indexPromise = new Promise ((resolve, reject) => {
         Promise.all([loadPromise, mwlPromise]).then((results) => {
             var cardArray = results[0];
+            var liteArray = [];
             var mwl = results[1];
             cardArray.forEach((card) => {
+                var liteCard = {
+                    title: card.title,
+                    text: card.text,
+                    code: card.code,
+                    uniqueness: card.uniqueness,
+                    side: card.side_code,
+                    faction: card.faction_code,
+                    type: card.type_code.replace(/^(ice|[a-z])/, (c) => {return c.toUpperCase();}),
+                    subtype: card.keywords,
+                    factioncost: card.faction_cost,
+                    baselink: card.base_link,
+                    cost: card.cost,
+                    memoryunits: card.memory_,
+                    strength: card.strength,
+                    trash: card.trash,
+                    advancementcost: card.advancement_cost,
+                    minimumdecksize: card.minimum_deck_size,
+                    influencelimit: card.influence_limit,
+                    agendapoints: card.agenda_points,
+                    url: nrdbCardDisplayURL + card.code
+                }
                 // Add a mwl flag to each card on the Most Wanted List.
                 if(mwl[card.code]) {
-                    card.mwl = mwl[card.code];
+                    liteCard.mwl = mwl[card.code];
                 }
                 // Add the card to the card sparse array for quick lookup.
-                cards[card.code] = card;
+                cards[card.code] = liteCard;
+                liteArray.push({title: card.title, code: card.code});
             });
             // Index the CardArray.
-            fuzzySearch = new FuzzySearch(cardArray, {
+            fuzzySearch = new FuzzySearch(liteArray, {
                 'caseSensitive': false,
                 'termPath': 'title',
                 'minimumScore': 200
@@ -222,7 +245,7 @@ function init (cardArray) {
             // fuzzySearch.addModule(SiftFS({'maxDistanceTolerance': 4, 'factor': 1}));
             fuzzySearch.addModule(LevenshteinFS({'maxDistanceTolerance': 4, 'factor': 1}));
             console.log('Cards indexed');
-            resolve(cardArray);
+            resolve(liteArray);
         }, (error) => {
             reject (error);
         });
