901ed0c64c770bcf738599cde943e5bb8bfc277c
Dominic Shelton <dominic.shelton@myob.com>
2017-07-21 17:44:11 +1200

    Updated to use the new 2.0 api

--- resizer.py
+++ resizer.py
@@ -4,22 +4,23 @@ import requests, json, threading, math
 from queue import Queue
 from io import BytesIO
 
-NUM_THREADS = 16
+NUM_THREADS = 24
 NRDB_URL = "http://netrunnerdb.com"
+IMG_DIR = "/card_image/"
 OUTPUT_FOLDER = "./thumbs/"
 
 crops = {
-        "Agenda":(59, 43, 270, 226),
-        "Asset":(44, 12, 256, 195),
-        "Corp":(22, 69, 277, 290),
-        "Event":(41, 43, 258, 230),
-        "Hardware":(42, 17, 258, 205),
-        "ICE":(63, 218, 238, 419),
-        "Operation":(44, 48, 255, 231),
-        "Program":(70, 25, 238, 171),
-        "Resource":(71, 34, 230, 171),
-        "Runner":(27, 55, 274, 269),
-        "Upgrade":(31, 0, 271, 209),
+        "agenda":(59, 43, 270, 226),
+        "asset":(44, 12, 256, 195),
+        "corp":(22, 69, 277, 290),
+        "event":(41, 43, 258, 230),
+        "hardware":(42, 17, 258, 205),
+        "ice":(63, 218, 238, 419),
+        "operation":(44, 48, 255, 231),
+        "program":(70, 25, 238, 171),
+        "resource":(71, 34, 230, 171),
+        "runner":(27, 55, 274, 269),
+        "upgrade":(31, 0, 271, 209),
         }
 
 q = Queue()
@@ -28,43 +29,49 @@ def worker ():
     while True:
         card = q.get()
 
-        if card["type"] == "Identity":
-            crop = crops[card["side"]]
+        if card["type_code"] == "identity":
+            crop = crops[card["side_code"]]
         else:
-            crop = crops[card["type"]]
+            crop = crops[card["type_code"]]
 
-        res = requests.get(NRDB_URL + card["imagesrc"])
-        im = Image.open(BytesIO(res.content))
-        im = im.crop(crop)
+        res = requests.get(NRDB_URL + IMG_DIR + card["code"] + ".png")
+        print(threading.current_thread().name + ":\t" + card["code"] + " fetched " + str(res.status_code))
+        if res.ok:
+            im = Image.open(BytesIO(res.content))
+            im = im.crop(crop)
 
-        if card["type"] == "ICE":
-            im = im.transpose(Image.ROTATE_270)
+            if card["type_code"] == "ice":
+                im = im.transpose(Image.ROTATE_270)
 
-        mask = Image.new("L", (im.width * 3, im.height * 3), 0)
-        hexagon = [
-                (0, mask.height / 2),
-                (mask.width / 4, 0),
-                (mask.width / 4 * 3, 0),
-                (mask.width, mask.height / 2),
-                (mask.width / 4 * 3, mask.height),
-                (mask.width / 4, mask.height)]
-        maskdraw = ImageDraw.Draw(mask)
-        maskdraw.polygon(hexagon, fill = 255);
-        mask = mask.resize(im.size, Image.ANTIALIAS)
+            im.width, im.height = im.size
+            mask = Image.new("L", (im.width * 3, im.height * 3), 0)
+            mask.width, mask.height = mask.size
+            hexagon = [
+                    (0, mask.height / 2),
+                    (mask.width / 4, 0),
+                    (mask.width / 4 * 3, 0),
+                    (mask.width, mask.height / 2),
+                    (mask.width / 4 * 3, mask.height),
+                    (mask.width / 4, mask.height)]
+            maskdraw = ImageDraw.Draw(mask)
+            maskdraw.polygon(hexagon, fill = 255);
+            mask = mask.resize(im.size, Image.ANTIALIAS)
 
-        output = Image.new("RGBA", (im.width, im.width), color = (255, 255, 255, 0))
-        output.paste(im,
-            box = (0, round((1 - (math.sqrt(3)/2)) / 2 * im.width)),
-            mask = mask)
+            output = Image.new("RGBA", (im.width, im.width), color = (255, 255, 255, 0))
+            output.paste(im,
+                box = (0, round((1 - (math.sqrt(3)/2)) / 2 * im.width)),
+                mask = mask)
 
-        output.save(OUTPUT_FOLDER + card["code"] + ".png", compress_level=9)
+            output.save(OUTPUT_FOLDER + card["code"] + ".png", compress_level=9)
+            print(threading.current_thread().name + ":\t" + card["title"] + " processed")
+
         q.task_done()
-        print(threading.current_thread().name + ":\t" + card["title"] + " processed")
 
 def main ():
     print("Fetching card list")
-    res = requests.get(NRDB_URL + "/api/cards")
-    cards = res.json()
+    res = requests.get(NRDB_URL + "/api/2.0/public/cards")
+    print(res)
+    cards = res.json()["data"]
 
     print("Spawning threads")
     for i in range(NUM_THREADS):
